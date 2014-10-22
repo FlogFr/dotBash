052c2058248350911a34d8091d96146ea1629f7e
aRkadeFR <contact@arkade.info>
2014-10-22 23:43:04 +0200

    update rc lua sound

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -368,8 +368,8 @@ globalkeys = awful.util.table.join(
     -- Media
     awful.key({ modkey,           }, "F1", function () awful.util.spawn("xscreensaver-command -lock") end), -- 
 
-    awful.key({ }, "XF86AudioLowerVolume",  function () awful.util.spawn("amixer -q sset Master 500-") end), --  
-    awful.key({ }, "XF86AudioRaiseVolume",  function () awful.util.spawn("amixer -q sset Master 500+") end), -- 
+    awful.key({ }, "XF86AudioLowerVolume",  function () awful.util.spawn("amixer -q sset Master 2-db") end), --  
+    awful.key({ }, "XF86AudioRaiseVolume",  function () awful.util.spawn("amixer -q sset Master 2+db") end), -- 
     awful.key({ }, "XF86AudioPlay",         function () awful.util.spawn("mpc toggle") end), -- 
     awful.key({ }, "XF86AudioMute",         function () awful.util.spawn("amixer -q sset Master toggle") end), -- 
     awful.key({ }, "XF86AudioNext",         function () awful.util.spawn("mpc next") end), -- 
