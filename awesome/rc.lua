f16a3c22ad8249e3f6814299bbf60be149479b5d
aRkadeFR <contact@arkade.info>
2015-03-24 20:35:29 +0100

    adding steam on float window

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -481,6 +481,8 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "MPlayer" },
       properties = { floating = true } },
+    { rule = { class = "Steam" },
+      properties = { floating = true } },
     { rule = { class = "pinentry" },
       properties = { floating = true } },
     { rule = { class = "Gimp" },
